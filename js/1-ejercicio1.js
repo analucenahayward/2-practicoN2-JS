a210a799cbfa4814c2155e8d9b8f923b40ba8b78
Ana <anitalucenahayward@gmail.com>
2022-08-01 20:57:58 -0300

    hice ejercicio1

---
+++ js/1-ejercicio1.js
@@ -0,0 +1,15 @@
+// que te pida la edad y si es mayor de 18 años que muestre un msj que si puede conducir
+
+let edad = prompt('Introduce tu edad');
+
+if (Number(edad)==edad){
+
+}
+
+if(edad>=18){
+    document.write('Puedes conducir');
+}
+
+else{
+    alert('Introduce un numero valido');
+}
