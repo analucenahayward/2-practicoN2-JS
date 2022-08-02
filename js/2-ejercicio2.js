87c0d1272b5d21044c42f9bad9d704a63f14262d
Ana <anitalucenahayward@gmail.com>
2022-08-01 21:28:14 -0300

    hice ejercicio2

---
+++ js/2-ejercicio2.js
@@ -0,0 +1,34 @@
+do{
+    let nota = prompt('Introduce tu nota');
+    if(Number(nota)==nota){
+        if(nota>0 && nota<=10){
+            if(nota<2){
+                alert('Muy deficiente')
+            }
+            else if(nota<4){
+                alert('Insuficiente')
+            }
+            else if(nota<6){
+                alert('Suficiente')
+            }
+            else if(nota<7){
+                alert('Bien')
+            }
+            else if(nota<9){
+                alert('Notable')
+            }
+            else if(nota=10){
+                alert('Sobresaliente')
+            }
+        }
+        else{
+            alert('Nota erronea');
+        }
+    }
+    else {
+        if (nota != undefined) {
+            alert("Introduce un numero valido");
+        }
+    }
+
+} while (nota != undefined);
